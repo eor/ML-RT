2d58583f963cc3a2130195f8ba1c94a135e0ff86
eor <eor@users.noreply.github.com>
2021-07-17 16:02:21 +0100

    MLP

---
+++ mlp/models.py
@@ -0,0 +1,40 @@
+from torch import nn
+#import torch.nn.functional as F
+import torch
+
+
+class MLP(nn.Module):
+    def __init__(self, conf):
+        super(MLP, self).__init__()
+
+        def block(features_in, features_out, normalise=conf.batch_norm, dropout=conf.dropout):
+
+            layers = [nn.Linear(features_in, features_out)]
+
+            # Different order of BN, Dropout, and non-linearity should be explored!
+            # From the literature it seems like there is no canonical way of doing it.
+
+            if normalise:
+                layers.append(nn.BatchNorm1d(features_out, 0.8))
+
+            if dropout:
+                layers.append(nn.Dropout(conf.dropout_value))
+
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+
+            return layers
+
+        self.model = nn.Sequential(
+            *block(conf.n_parameters, 64, normalise=False, dropout=False),
+            *block(64, 128),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(conf.profile_len)),
+
+        )
+
+    def forward(self, parameters):
+
+        return self.model(parameters)
+
