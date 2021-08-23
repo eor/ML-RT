765b363a805d2ab8d96b4e2539016189db9074cd
eor <eor@users.noreply.github.com>
2021-08-23 16:21:30 +0100

    renamed npy files, added Helium II & III profiles

--- data/053_data_set/get_data.sh
+++ data/053_data_set/get_data.sh
@@ -4,21 +4,25 @@
 
 BASEURL="https://www.astro.rug.nl/~krause/static/ML-RT/053_data_set/"
 
-FILE1="data_Hprofiles.npy"
-FILE2="data_Tprofiles.npy"
+FILE1="data_H_profiles.npy"
+FILE2="data_T_profiles.npy"
 FILE3="data_parameters.npy"
+FILE4="data_HeII_profiles.npy"
+FILE5="data_HeIII_profiles.npy"
 
 CHECKSUM1="62375d3a72ee14f12e1d96a589d3c1de"
 CHECKSUM2="bfbfbb279b41aed19c39d933633a67e3"
 CHECKSUM3="a8e1de335d896a7fcd573d401836a23e"
-
-
+CHECKSUM4="2c2bd60cbd837aaa42a9391d4c1d1a10"
+CHECKSUM5="0ed606f9bc78e175583372ae203c0cbc"
 
 dl() {
 
         wget -c $BASEURL$FILE1
         wget -c $BASEURL$FILE2
         wget -c $BASEURL$FILE3
+        wget -c $BASEURL$FILE4
+        wget -c $BASEURL$FILE5
 }
 
 
@@ -27,7 +31,9 @@ check() {
         VAR1=$(md5sum ${FILE1} | awk '{print $1}')
         VAR2=$(md5sum ${FILE2} | awk '{print $1}')
         VAR3=$(md5sum ${FILE3} | awk '{print $1}')
-        
+        VAR4=$(md5sum ${FILE4} | awk '{print $1}')
+        VAR5=$(md5sum ${FILE5} | awk '{print $1}')
+
         # 1
         if [ "$CHECKSUM1" = "$VAR1" ]; then
 
@@ -56,7 +62,24 @@ check() {
         else
             echo "Checksum NOT ok: "$FILE3
         fi        
-        
+
+        # 4
+        if [ "$CHECKSUM4" = "$VAR4" ]; then
+
+            echo "Checksum OK: "$FILE4
+
+        else
+            echo "Checksum NOT ok: "$FILE4
+        fi
+
+        # 5
+        if [ "$CHECKSUM5" = "$VAR5" ]; then
+
+            echo "Checksum OK: "$FILE5
+
+        else
+            echo "Checksum NOT ok: "$FILE5
+        fi
 }
 
 
